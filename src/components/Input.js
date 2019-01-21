fd4dfe9efc12f4b4db72d433abe7e9295383fdc2
Sava <sava.vuckovic@quantox.com>
2019-01-21 11:23:52 +0100

    created component boilerplates

---
+++ src/components/Input.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Input = () => (
+  <input type="text"/>
+)
+
+export default Input;
---
+++ src/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ListItem from './ListItem';
+
+const List = () => (
+  <ul>
+    <ListItem />
+    <ListItem />
+    <ListItem />
+    <ListItem />
+    <ListItem />
+  </ul>
+);
+
+export default List;
