3d70863a839fe975cafd70cf392535e6b052dc4a
Sava <sava.vuckovic@quantox.com>
2019-01-21 12:46:39 +0100

    added logic to check if todo list is empty

--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import ListItem from './ListItem';
 
-const List = ({ todos, removeTodo }) => (
-  <ul>
-    {todos.map((item, i) => (
-      <ListItem removeTodo={removeTodo} text={item} key={i}/>
-    ))}
-  </ul>
-);
+const List = ({ todos, removeTodo }) => {
+  if (todos.length === 0) {
+    return <h1>No items in to-do list</h1>;
+  } else {
+    return (
+      <ul>
+        {todos.map((item, i) => (
+          <ListItem removeTodo={removeTodo} text={item} key={i}/>
+        ))}
+      </ul>
+    );
+  }
+}
 
 export default List;
